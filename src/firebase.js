4c8984750ddec9b8c5b416d54cd872fec473671d
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-25 22:48:50 +0200

    Creating firebase script

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAmj61UdKPQwONnZDFD_A-NSYmAd5-T4AU",
+    authDomain: "chat-app005.firebaseapp.com",
+    projectId: "chat-app005",
+    storageBucket: "chat-app005.appspot.com",
+    messagingSenderId: "783252173320",
+    appId: "1:783252173320:web:b98f8b7006b98b9c4657bd",
+    measurementId: "G-0KRXRSRBD1"
+})
+
+
+const db=firebaseApp.firestore()
+
+const auth=firebase.auth()
+  
+export {db, auth}
