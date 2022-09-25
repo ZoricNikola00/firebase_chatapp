64620738576bf744a81c08a62a5dc68abf5e9178
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-25 23:22:26 +0200

    Minor fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAmj61UdKPQwONnZDFD_A-NSYmAd5-T4AU",
