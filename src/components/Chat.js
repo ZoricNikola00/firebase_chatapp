63aa9a5d66063c4e8f59455f1832d0223014e95c
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-25 23:43:12 +0200

    Creating chat container

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { auth, db } from '../firebase'
+import SignOut from './SignOut'
 
 const Chat = () => {
+    const [messages, setMessages] = useState([])
+
+    useEffect(()=>{
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        
+        })
+    },[])
+    console.log(messages)
   return (
-    <div>Chat</div>
+    <div className='chatContainer'>
+        <SignOut/>
+        <div className='chat'>
+            <div className='msgs'>
+            {messages.map((x,id)=>{
+                const {uid,text,photoURL}=x
+                return <div  key={id} className={`msgCont ${uid===auth.currentUser.uid?'user':'other'}`}>
+                    <div className={`msg ${uid===auth.currentUser.uid?'user':'other'}`} >
+                        <p>{text}</p>
+                        <img src={photoURL}/>              
+                    </div>
+                </div>
+        })}
+            </div>
+        </div>
+    </div>
   )
 }
 
